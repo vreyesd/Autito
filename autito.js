6928b755ebf5e31d82f19fd74d25e2d7b6294d4b
Víctor Reyes <victorh.reyesd@gmail.com>
2013-11-29 21:14:11 -0300

    Identation

--- autito.js
+++ autito.js
@@ -1,71 +1,70 @@
-
-function paintAutito()
-{
+'uset strict';
+function Autito(x, y) {
+    this.x = (x === null) ? 0 : x;
+    this.y = (y === null) ? 0 : y;
+    this.width = 30;
+    this.height = 10;
+    this.speed = 0;
+    this.accel = 0.5;
+    this.breaks = 3;
+    this.angle = 0;
+    this.turning = 0.3;
+    this.max_speed = 30;
+    this.name = "Fittipaldi";
+      
+    this.move = function (PRESSING) {
+        if (PRESSING[37]) {
+            this.angle += -this.speed * this.turning;
+        }
+        if (PRESSING[38]) {
+            if (this.speed < this.max_speed) {
+                this.speed += this.accel; //up
+            }
+        } else {
+            if (Math.abs(this.speed) > 0) {
+                this.speed = Math.abs(this.speed) * 0.8;
+            }
+        }
+        if (PRESSING[39]) {
+            this.angle += this.speed * this.turning;
+        }
+        if (PRESSING[40]) {
+            if (Math.abs(this.speed) >= this.breaks) {
+                this.speed = Math.abs(this.speed) - this.breaks;
+            } else {
+                this.speed -= this.speed;
+            }
+        }
+      
+        this.x += Math.cos(this.angle * Math.PI / 180) * this.speed;
+        this.y += Math.sin(this.angle * Math.PI / 180) * this.speed;
+        return this.speed;
+    };
    
-   ctx.save();
-   ctx.fillStyle = "#0f0";
-   ctx.translate(fittipaldi.x, fittipaldi.y);
-   ctx.rotate(fittipaldi.angle*Math.PI/180);
-   ctx.fillRect(-0.5*fittipaldi.width, -0.5*fittipaldi.height, fittipaldi.width, fittipaldi.height);
+    this.paint = function (ctx) {
+        ctx.save();
+        ctx.fillStyle = "#0f0";
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.angle * Math.PI / 180);
+        ctx.fillRect(-0.5 * this.width, -0.5 * this.height, this.width, this.height);
    
-   drawWheel( -0.5*fittipaldi.height+3, -0.5*fittipaldi.width+9.5);
-   drawWheel( -0.5*fittipaldi.height+19.5, -0.5*fittipaldi.width+9.5);
-   drawWheel( -0.5*fittipaldi.height+3, -0.5*fittipaldi.width+20.6);
-   drawWheel( -0.5*fittipaldi.height+19.5, -0.5*fittipaldi.width+20.6);
+        this.drawWheel(-0.5 * this.height + 3, -0.5 * this.width + 9.5, ctx);
+        this.drawWheel(-0.5 * this.height + 19.5, -0.5 * this.width + 9.5, ctx);
+        this.drawWheel(-0.5 * this.height + 3, -0.5 * this.width + 20.6, ctx);
+        this.drawWheel(-0.5 * this.height + 19.5, -0.5 * this.width + 20.6, ctx);
       
-   ctx.restore();
-}
-
-
-function Autito(x, y) 
-{
-   this.x=(x==null)?0:x;
-   this.y=(y==null)?0:y;
-   this.width=30;
-   this.height=10;
-   this.speed=0;
-   this.accel=0.5;
-   this.break=3;
-   this.angle=0;
-   this.max_speed=30;
-   this.name="Fittipaldi";
-      
-   this.move=function()
-   {
-      if(PRESSING[37]) this.angle += -2.0; 
-      if(PRESSING[38])  
-      {
-         if(this.speed < this.max_speed)
-            this.speed += this.accel; //up
-      }
-      else
-      {
-         if(Math.abs(this.speed) > 0 ) this.speed = Math.abs(this.speed)*0.8;
-      }
-      if(PRESSING[39]) this.angle += 2.0;
-      if(PRESSING[40])
-      {
-         if(Math.abs(this.speed) >= this.break)
-            this.speed = Math.abs(this.speed)-this.break;
-         else
-            this.speed -= this.speed;
-      }
-      
-      this.x += Math.cos(this.angle*Math.PI/180)*this.speed;
-      this.y += Math.sin(this.angle*Math.PI/180)*this.speed;
-      return this.speed;
-    }   
-}
-
-function drawWheel(x, y)
-{
-   ctx.beginPath();
-   ctx.moveTo(x,y);
-   ctx.bezierCurveTo(x,y+1.3, x-2.9, y+2.3, x-6.4, y+2.3);
-   ctx.bezierCurveTo(x-9.9, y+2.3, x-12.7, y+1.3, x-12.7, y);
-   ctx.bezierCurveTo(x-12.7, y-1.2, x-9.9, y-2.3, x-6.4, y-2.3);
-   ctx.bezierCurveTo(x-2.9, y-2.3, x, y-1.2, x, y);
-   ctx.closePath();
-   ctx.fillStyle="black";
-   ctx.fill();
+        ctx.restore();
+    };
+    
+    this.drawWheel = function (x, y, ctx) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+        ctx.bezierCurveTo(x, y + 1.3, x - 2.9, y + 2.3, x - 6.4, y + 2.3);
+        ctx.bezierCurveTo(x - 9.9, y + 2.3, x - 12.7, y + 1.3, x - 12.7, y);
+        ctx.bezierCurveTo(x - 12.7, y - 1.2, x - 9.9, y - 2.3, x - 6.4, y - 2.3);
+        ctx.bezierCurveTo(x - 2.9, y - 2.3, x, y - 1.2, x, y);
+        ctx.closePath();
+        ctx.fillStyle = "black";
+        ctx.fill();
+    };
 }
--- game.js
+++ game.js
@@ -1,65 +1,61 @@
 'use strict';
-var PRESSING=[];
+var PRESSING = [];
 var fittipaldi;
 var canvas;
 var ctx;
-var lastPress=null;
+var lastPress = null;
 
-function init()
-{
+function init() {
     canvas = document.getElementById("canvas");
     window.addEventListener('resize', resizeCanvas, false);
-    if (canvas.getContext)
-    {
+    if (canvas.getContext) {
         ctx = canvas.getContext("2d");
         resizeCanvas();
         fittipaldi  = new Autito(80, 80);
         run();
     }
-    function resizeCanvas(){
+    function resizeCanvas() {
         var head = document.getElementById('header').offsetHeight;
         canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight-head;
+        canvas.height = window.innerHeight - head;
     }
 }
 
-function run()
-{
-   setTimeout(run,50);
-   move();
-   paint();
+function run() {
+    setTimeout(run, 50);
+    move();
+    paint();
 }
 
-function move()
-{
-   fittipaldi.move();
+function move() {
+    fittipaldi.move(PRESSING);
 }
 
-function paint()
-{
-   ctx.clearRect(0,0,canvas.width,canvas.height);
-   ctx.fillStyle = "black";
-   ctx.rect(0, 0, 300, 150);
+function paint() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "black";
+    ctx.rect(0, 0, 300, 150);
    
-   ctx.fillStyle='#fff';
-   ctx.fillText('Pilot: '+fittipaldi.name,5,10);
-   ctx.fillText('Last Press: '+lastPress,5,25);
+    ctx.fillStyle = '#fff';
+    ctx.fillText('Pilot: ' + fittipaldi.name, 5, 10);
+    ctx.fillText('Last Press: ' + lastPress, 5, 25);
    
-   //data
-   ctx.fillText('Speed: '+fittipaldi.speed,150,10);
-   ctx.fillText('Accel: '+fittipaldi.accel,150,20);
-   ctx.fillText('x, y: '+fittipaldi.x+" "+fittipaldi.y,150,30);
-   ctx.fillText('Angle: '+(fittipaldi.angle*(180/Math.PI)).toFixed(1),150,40);  
+    //data
+    ctx.fillText('Speed: ' + fittipaldi.speed, 150, 10);
+    ctx.fillText('Accel: ' + fittipaldi.accel, 150, 20);
+    ctx.fillText('x, y: ' + fittipaldi.x + " " + fittipaldi.y, 150, 30);
+    ctx.fillText('Angle: ' + (fittipaldi.angle * (180 / Math.PI)).toFixed(1), 150, 40);
    
-   paintAutito();
+    //paintAutito();
+    fittipaldi.paint(ctx);
 }
 
 
-document.addEventListener('keydown',function(evt){
- lastPress=evt.keyCode;
- PRESSING[evt.keyCode]=true;
-},false);
+document.addEventListener('keydown', function (evt) {
+    lastPress = evt.keyCode;
+    PRESSING[evt.keyCode] = true;
+}, false);
 
-document.addEventListener('keyup',function(evt){
- PRESSING[evt.keyCode]=false;
-},false);
+document.addEventListener('keyup', function (evt) {
+    PRESSING[evt.keyCode] = false;
+}, false);
